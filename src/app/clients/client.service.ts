26a6938e4a5e72ccddedeea74fa07d2dbff4b0db
luisdev97 <amadormontorodavid@gmail.com>
2019-12-30 23:25:27 +0100

    Show ClientData and img in ClientDetailComponent

--- src/app/clients/client.service.ts
+++ src/app/clients/client.service.ts
@@ -104,7 +104,7 @@ export class ClientService {
     let formData = new FormData();
     formData.append("file", file);
     formData.append("id", id);
-    return this.http.post(`${this.urlEndPoint}/upload/`, formData).pipe(
+    return this.http.post(`${this.urlEndPoint}/upload`, formData).pipe(
       map ((response: any) => response.client as Client),
       catchError(e => {
          swal.fire(e.error.message, e.error.preciseMessage , 'error');
