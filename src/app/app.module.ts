ed679f334fd13e0943111ef645d839969f31a475
luisdev97 <amadormontorodavid@gmail.com>
2020-01-19 21:11:52 +0100

    Guard router has role

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ import { MatDatepickerModule } from '@angular/material';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { ClientsDetailComponent } from './clients-detail/clients-detail.component';
 import { LoginComponent } from './users/login.component';
-import { AuthGuard } from './users/grs/guards/auth.guard';
+import { AuthGuard } from './users/guards/auth.guard';
+import { RoleGuard } from './users/guards/role.guard';
 
 registerLocaleData(localeEn, 'en');
 
@@ -27,8 +28,8 @@ const routes: Routes = [
   { path: 'home' , redirectTo: '/clients' , pathMatch: 'full' },
   { path: 'clients', component: ClientsComponent },
   { path: 'clients/page/:page', component: ClientsComponent },
-  { path: 'clients/form', component: FormComponent, canActivate: [AuthGuard]},
-  { path: 'clients/form/:id', component: FormComponent, canActivate: [AuthGuard] },
+  { path: 'clients/form', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'ROLE_ADMIN'}},
+  { path: 'clients/form/:id', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'ROLE_ADMIN'}},
   { path: 'login', component: LoginComponent },
 ];
 
--- src/app/users/guards/role.guard.ts
+++ src/app/users/guards/role.guard.ts
@@ -1,27 +1,35 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from '../../auth.service';
+import { AuthService } from '../auth.service';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class RoleGuard implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router){
-    
+
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     
-     if(this.authService.isAuthenticated()){
+    if(!this.authService.isAuthenticated()){
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    const role: string = next.data['role'] as string;
+    if(this.authService.hasRole(role)){
       return true;
     }
 
-    this.router.navigate(['/login']);
-    return false;
+    Swal.fire("Denied access", "You do not have access to this resource", 'warning');
+    this.router.navigate(['/clients']);
+    return true;
   }
   
 }
